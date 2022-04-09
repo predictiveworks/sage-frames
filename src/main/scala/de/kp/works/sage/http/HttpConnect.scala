6abf9a936ebba9d31c7fae22b22b461317d1d536
Predictive Works <info@predictiveworks.eu>
2022-04-09 16:55:09 +0200

    Project updated

--- src/main/scala/de/kp/works/sage/http/HttpConnect.scala
+++ src/main/scala/de/kp/works/sage/http/HttpConnect.scala
@@ -19,24 +19,26 @@ package de.kp.works.sage.http
  *
  */
 
+import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.ContentTypes._
 import akka.http.scaladsl.model.headers._
-import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.{ActorMaterializer, KillSwitches}
+import akka.stream.scaladsl.{RestartSource, Sink, Source}
 import akka.util.{ByteString, Timeout}
 import com.google.gson._
 
 import scala.collection.JavaConversions.asScalaSet
 import scala.concurrent.{Await, ExecutionContextExecutor, Future}
 import scala.concurrent.duration._
+import scala.util.{Failure, Success, Try}
 
 trait HttpConnect {
 
   private val uuid = java.util.UUID.randomUUID.toString
-  implicit val httpSystem: ActorSystem = ActorSystem(s"http-system-$uuid")
+  implicit val httpSystem: ActorSystem = ActorSystem(s"http-connect-$uuid")
 
   implicit lazy val httpContext: ExecutionContextExecutor = httpSystem.dispatcher
   implicit val httpMaterializer: ActorMaterializer = ActorMaterializer()
@@ -46,14 +48,16 @@ trait HttpConnect {
    */
   val duration: FiniteDuration = 10.seconds
   implicit val timeout: Timeout = Timeout(duration)
-
-  def getHtml(endpoint:String):String = {
-    extractHtmlBody(get(endpoint))
-  }
-
-  def getJson(endpoint:String):JsonElement = {
-    extractJsonBody(get(endpoint))
-  }
+  /**
+   * Leveraging `superPool`, you get back a Flow which will make sure
+   * that it will not take more requests than the pool is able to handle,
+   * because of the backpressure.
+   */
+  private val pool = Http(httpSystem).superPool[NotUsed]()
+  /**
+   * A shared kill switch used to terminate the *entire stream* if needed
+   */
+  private val killSwitch = KillSwitches.shared("killSwitch")
 
   def extractTextBody(source:Source[ByteString,Any]):String = {
 
@@ -156,8 +160,11 @@ trait HttpConnect {
     }
 
   }
-
-  def get(endpoint:String, headers:Map[String,String]=Map.empty[String,String]):Source[ByteString,Any] = {
+  /**
+   * A public GET method that either leverages the Akka
+   * `singleRequest` or the `pooled` API
+   */
+  def get(endpoint:String, headers:Map[String,String], pooled:Boolean = false):Source[ByteString,Any] = {
 
     try {
 
@@ -167,24 +174,113 @@ trait HttpConnect {
 
         else
           HttpRequest(HttpMethods.GET, endpoint, headers=headers.map{case(k,v) => RawHeader(k, v)}.toList)
-
       }
-      val future: Future[HttpResponse] = Http(httpSystem).singleRequest(request)
-      val response = Await.result(future, duration)
 
-      val status = response.status
-      if (status == StatusCodes.OK)
-        return response.entity.withoutSizeLimit().dataBytes
+      val response = Await.result({
+        if (pooled) {
+          pooledGet(request)
 
-      if (status == StatusCodes.UnprocessableEntity)
-        return response.entity.dataBytes
+        } else singleGet(request)
 
-      throw new Exception(s"Request to Http endpoint returns with: ${status.value}.")
+      }, duration)
+
+      response
 
     } catch {
       case t:Throwable =>
         throw new Exception(t.getLocalizedMessage)
     }
+
+  }
+  /**
+   * The `backoff` policy for pooled GET requests
+   */
+  def backoff:BackoffPolicy = throw new Exception("Not implemented.")
+  /**
+   * The `parallelism` of pooled GET requests
+   */
+  def parallelism:Int = 1
+
+  private def singleGet(request:HttpRequest):Future[Source[ByteString,Any]] = {
+    Http(httpSystem)
+      .singleRequest(request)
+      .map(handleSingleResponse)
+  }
+
+  private def handleSingleResponse:PartialFunction[HttpResponse, Source[ByteString,Any]] =  {
+    /*
+     * Handling regular Http responses
+     */
+    case HttpResponse(StatusCodes.OK, _, entity, _) =>
+      entity.withoutSizeLimit().dataBytes
+
+    case HttpResponse(StatusCodes.UnprocessableEntity,_,entity,_) =>
+      entity.dataBytes
+    /*
+     * Handling (429) Too many requests, and apply
+     * exponential backoff
+     */
+    case HttpResponse(StatusCodes.TooManyRequests,_,entity,_) =>
+      entity.discardBytes()
+      throw TooManyRequests
+
+    case HttpResponse(statusCode, _, entity, _) =>
+      entity.discardBytes()
+      killSwitch.shutdown()
+
+      throw new Exception(s"Request to Http endpoint returns with: ${statusCode.value}.")
+
+  }
+  /**
+   * This method supports GET requests with `backoff` support,
+   * and also enables proper handling of `Too many requests`.
+   *
+   * The [Backoff] must be adjusted to the selected REST API
+   * like Shopify, Sage, (Open)Weather etc.
+   */
+  private def pooledGet(request:HttpRequest):Future[Source[ByteString,Any]] = {
+    /*
+     * Perform connection pool based Http GET request; it uses
+     * the `backoff` feature to supervise a stream and restart
+     * it with exponential backoff.
+     */
+    RestartSource.withBackoff(
+      minBackoff = backoff.minBackoff, maxBackoff = backoff.maxBackoff, randomFactor = backoff.randomFactor, maxRestarts = backoff.maxRestarts)
+    { () =>
+      Source.single((request, NotUsed)).via(pool)
+        .mapAsync(parallelism = parallelism)(handlePooledResponse)
+
+    }.runWith(Sink.head).recover {
+      case _ => throw FailedAfterMaxRetries
+    }
+
+  }
+
+  private def handlePooledResponse:PartialFunction[(Try[HttpResponse],NotUsed), Future[Source[ByteString,Any]]] =  {
+    case (Success(answer), _) => answer match {
+      /*
+       * Handling regular Http responses
+       */
+      case HttpResponse(StatusCodes.OK, _, entity, _) =>
+        Future {entity.withoutSizeLimit().dataBytes}
+
+      case HttpResponse(StatusCodes.UnprocessableEntity,_,entity,_) =>
+        Future {entity.dataBytes}
+      /*
+       * Handling (429) Too many requests, and apply
+       * exponential backoff
+       */
+      case HttpResponse(StatusCodes.TooManyRequests,_,entity,_) =>
+        entity.discardBytes()
+        throw TooManyRequests
+
+      case HttpResponse(statusCode, _, entity, _) =>
+        entity.discardBytes()
+        killSwitch.shutdown()
+
+        throw new Exception(s"Request to Http endpoint returns with: ${statusCode.value}.")
+    }
+    case (Failure(failure), _) => throw failure
 
   }
 
